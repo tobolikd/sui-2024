6a76c090851410cff827a141f5092c14a300a4f8
David Skrabal <xskrab11@stud.fit.vutbr.cz>
2024-12-08 10:33:58 +0100

    feat: add DFS with depth limit

--- proj3/sui-solution.cc
+++ proj3/sui-solution.cc
@@ -1,6 +1,7 @@
 #include "search-strategies.h"
 #include "memusage.h"
 #include <queue>
+#include <stack>
 #include <set>
 
 const size_t memory_limit = 49000000;
@@ -40,6 +41,38 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
+	std::stack<std::pair<SearchState, std::vector<SearchAction>>> stack;
+	std::set<SearchState> visited;
+
+	stack.push({init_state,{}});
+
+	while(!stack.empty()){
+		if(getCurrentRSS()>memory_limit){
+			return {};
+		}
+		auto [actual_state, actual_path]=stack.top();
+		stack.pop();
+
+		if(actual_state.isFinal()){
+			return actual_path;
+		}
+
+		if(actual_path.end()-actual_path.begin() >= depth_limit_)continue;
+
+		if(visited.find(actual_state)==visited.end()){
+			visited.insert(actual_state);
+
+			for(auto &action : actual_state.actions()){
+				SearchState new_state = action.execute(actual_state);
+				if(visited.find(new_state)==visited.end()){
+					auto new_path=actual_path;
+					new_path.push_back(action);
+					stack.push({new_state,new_path});
+				}
+			}
+		}
+
+	}
 	return {};
 }
 
